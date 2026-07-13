d4806956e94d5567cf846f2890583ffba2cc5db4
julenhurtado92-pixel <julenhurtado92@gmail.com>
2026-07-13 19:14:35 +0200

    Add files via upload

---
+++ docs/data/wishlist-data.js
@@ -0,0 +1,7 @@
+window.wishlistData = {
+  "metadata": {
+    "generatedAt": "2026-07-13T00:00:00+00:00",
+    "description": "Wish List inicial. Las altas desde la web se guardan en localStorage y se pueden exportar a este formato."
+  },
+  "items": []
+};
